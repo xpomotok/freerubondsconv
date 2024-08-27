3e639c572ef90c763a7a542c4bef3d876eeb7449
Sergey <sergey.a.kuzmin@gmail.com>
2024-08-27 19:12:06 +0300

    Небольшой рефакторинг в свободное время.

--- freerbcon.py
+++ freerbcon.py
@@ -1,50 +1,57 @@
 # from Queue import Queue
+from builtins import str
 
 params = 14
 sep = ";"
+endl = "\n"
 in_file = "rusbonds.txt"
 out_file = "rbondsconv.csv"
 
-header = ["Наименование","Статус", "Дата погашения", "Дата окончания размещения",
- "ДЭ%", "Объём эмиссии", "Ставка купона%", "Листинг Мос.биржи", "Сектор эмитента",
-  "Эмитент", "Дюрация лет", "Цена %", "К-во купонов в год", "Объём торгов"]
+header = ["Наименование", "Статус", "Дата погашения", "Дата окончания размещения",
+          "ДЭ%", "Объём эмиссии", "Ставка купона%", "Листинг Мос.биржи", "Сектор эмитента",
+          "Эмитент", "Дюрация лет", "Цена %", "К-во купонов в год", "Объём торгов"]
+
+
+def form_header(header_list) -> str:
+	head = sep.join(header_list)
+	head = "".join([head, endl])
+	return head
+
+
+def form_record(string) -> str:
+	out_string = string.replace(".", ",")
+	out_string = "".join([out_string, sep])
+	return out_string
 
 
 def convert():
-
-	image = []
 	# writeQueue = Queue()
 
 	try:
 		with open(in_file, "r", encoding="UTF-8") as f:
 			string = f.read()
 
-			image = string.split("\n")
-			try:
-				with open(out_file, "w") as f:
-					cnt = 0
-
-					for h in header:
-						f.write(h)
-						f.write(sep)
-
-					f.write("\n")
-
-					for i in image:
-						i = i.replace(".", ",")
-						f.write(i)
-						f.write(sep)
-
-						cnt += 1
-						if cnt == params:
-							cnt = 0
-							f.write("\n")
-				print("Written!")
-			except FileNotFoundError:
-				print("Converter: Couldn't create output file!")
+			# TODO следующий вариант с базой для более удобной сортировки и фильтрации
+			image = string.split(endl)
 	except FileNotFoundError:
 		print("Converter: Input file doesn't exist!")
+		return
 
+	try:
+		with open(out_file, "w") as f:
+			cnt = 0
+
+			f.write(form_header(header))
+			""" зная, сколько полей на одну запись, делаем в выходном файле новую строку """
+			for i in image:
+				f.write(form_record(i))
+				cnt += 1
+				if cnt == params:
+					cnt = 0
+					f.write(endl)
+		print("Converter: All records have been written!")
+	except FileNotFoundError:
+		print("Converter: Couldn't create output file!")
 
 
 if __name__ == "__main__":
