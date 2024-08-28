fd9d318cf63d4ed53bf1835082d3fe6c0f05d87a
xpomotok <39444310+xpomotok@users.noreply.github.com>
2024-08-28 20:13:24 +0300

    Добавлен прототип дата класса для облигации

--- freerbcon.py
+++ freerbcon.py
@@ -1,15 +1,34 @@
-# from Queue import Queue
+from dataclasses import dataclass
 from builtins import str
 
-params = 14
+
+params = 15 #14
 sep = ";"
 endl = "\n"
-in_file = "rusbonds.txt"
-out_file = "rbondsconv.csv"
+in_file = "Data/2024-08-bonds.txt"
+out_file = "Data/280824.csv"
+header = ["Наименование", "Сектор",  "ISIN", "Листинг", "Дата погашения",
+ 	"ДЭ%", "Дюрация лет", "Цена %", "Тип купона", "Ставка купона %", "Количество купонов",
+    "Объём торгов", "Вид обеспечения", "Рейтинг 1", "Рейтинг 2"]
 
-header = ["Наименование", "Статус", "Дата погашения", "Дата окончания размещения",
-          "ДЭ%", "Объём эмиссии", "Ставка купона%", "Листинг Мос.биржи", "Сектор эмитента",
-          "Эмитент", "Дюрация лет", "Цена %", "К-во купонов в год", "Объём торгов"]
+
+@dataclass
+class Bond():
+	title: str
+	sector: str
+	isin: str
+	listing: str
+	redemtion_date: str
+	de: float
+	duration: float
+	price: float
+	coupon_type: str
+	coupon_percent: float
+	coupon_total: int
+	trading_volume: float
+	security: str
+	rating1: str
+	rating2: str
 
 
 def form_header(header_list) -> str:
@@ -28,14 +47,27 @@ def convert():
 	# writeQueue = Queue()
 
 	try:
-		with open(in_file, "r", encoding="UTF-8") as f:
-			string = f.read()
-
-			# TODO следующий вариант с базой для более удобной сортировки и фильтрации
-			image = string.split(endl)
+		f = open(in_file, "r", encoding="UTF-8")
 	except FileNotFoundError:
 		print("Converter: Input file doesn't exist!")
 		return
+	else:
+		with f:
+			string = f.read()
+	
+	# TODO следующий вариант с базой для более удобной сортировки и фильтрации
+	raw_string_list = string.split(endl)
+
+	if len(raw_string_list) % params != 0:
+		print("Converter: Raw data is unaligned!")
+		return
+	
+	bonds = []
+
+	# for i in range(0, len(raw_string_list), params):
+	# 	bond = Bond(raw_string_list[i:params])
+	# 	bonds.append(bond)
+	# 	print(bond)
 
 	try:
 		with open(out_file, "w") as f:
@@ -43,14 +75,15 @@ def convert():
 
 			f.write(form_header(header))
 			""" зная, сколько полей на одну запись, делаем в выходном файле новую строку """
-			for i in image:
+			for i in raw_string_list:
 				f.write(form_record(i))
 				cnt += 1
 				if cnt == params:
 					cnt = 0
 					f.write(endl)
 		print("Converter: All records have been written!")
-	except FileNotFoundError:
+
+	except PermissionError:
 		print("Converter: Couldn't create output file!")
 
 
