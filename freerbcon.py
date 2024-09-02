4f780dbfecabbfb8700a105cc58f4b3a00d3bbfc
Sergey <sergey.a.kuzmin@gmail.com>
2024-09-02 23:51:04 +0300

    Небольшой рефакторинг в свободное время.

--- freerbcon.py
+++ freerbcon.py
@@ -11,51 +11,106 @@ header = ["Наименование", "Сектор", "ISIN", "Листинг", 
           "Объём торгов", "Вид обеспечения", "Рейтинг 1", "Рейтинг 2"]
 
 
-# @dataclass
-class Bond():
-    # title: str
-    # sector: str
-    # isin: str
-    # listing: str
-    # redemtion_date: str
-    # de: str
-    # duration: float
-    # price: str #float
-    # coupon_type: str
-    # coupon_percent: float
-    # coupon_total: int
-    # trading_volume: float
-    # security: str
-    # rating1: any
-    # rating2: any
+class RawData:
+    raw_string_list: list
 
-    def __init__(self, bond_builder):
+    def __init__(self):
+        self.string = ""
+
+    def populate(self, in_file: str):
+        try:
+            f = open(in_file, "r", encoding="UTF-8")
+        except FileNotFoundError:
+            print("Converter: Input file doesn't exist!")
+            return
+        else:
+            with f:
+                self.string = f.read()
+
+    def split(self):
+        # TODO следующий вариант с базой для более удобной сортировки и фильтрации
+        self.raw_string_list = self.string.split(endl)
+
+        if len(self.raw_string_list) % params != 0:
+            print("Converter: Raw data is unaligned!")
+            return
+
+    def get_data(self) -> list:
+        return self.raw_string_list
+
+
+class CsvData:
+
+    def __init__(self, raw_string_list: list, params: int) -> None:
+        self.records = 0
+        self.bonds = []
+        bond_builder = BondBuilder()
+
+        """ зная, сколько полей на одну запись, делаем в выходном файле новую строку """
+        for i in range(0, len(raw_string_list), params):
+            raw_slice = raw_string_list[i:i + params]
+            bond = bond_builder.set_title(raw_slice[0]) \
+                .set_isin(raw_slice[1]) \
+                .set_listing(raw_slice[2]) \
+                .set_redemption_date(raw_slice[3]) \
+                .set_de(raw_slice[4]).build()
+
+            print(bond)
+            self.bonds.append(bond)
+            self.records += 1
+
+    def get_bonds(self) -> list:
+        return self.bonds
+
+    def export(self, out_file: str, header: list):
+        try:
+            with open(out_file, "w") as f:
+                f.write(form_record(header))
+                for b in self.bonds:
+                    f.write(str(b))
+
+                # for i in range(0, len(raw_string_list), params):
+                #     f.write(form_record(raw_string_list[i:i + params]))
+                #     cnt += 1
+
+            print("Converter: {} records have been written!".format(self.records))
+            print(self.bonds)
+
+        except PermissionError:
+            print("Converter: Couldn't create output file!")
+
+
+# @dataclass
+class Bond:
+
+    def __init__(self, builder):
         self.title = builder.title
-        self.sector= builder.sector
+        self.sector = builder.sector
         self.isin = builder.isin
-        self.listing= builder.listing
-        self.redemtion_date= builder.redemption_date
-        self.de= builder.de
-        self.duration= builder.duration
-        self.price= builder.price
-        self.coupon_type= builder.coupon_type
-        self.coupon_percent= builder.coupon_percent
-        self.coupon_total= builder.coupon_total
-        self.trading_volume= builder.trading_volume
-        self.security= builder.security
-        self.rating1= builder.rating1
-        self.rating2= builder.rating2
+        self.listing = builder.listing
+        self.redemption_date = builder.redemption_date
+        self.de = builder.de
+        self.duration = builder.duration
+        self.price = builder.price
+        self.coupon_type = builder.coupon_type
+        self.coupon_percent = builder.coupon_percent
+        self.coupon_total = builder.coupon_total
+        self.trading_volume = builder.trading_volume
+        self.security = builder.security
+        self.rating1 = builder.rating1
+        self.rating2 = builder.rating2
 
     def __str__(self):
-        return "{} {}".format(self.title, self.isin)
+        return "{};{};{};{};{}{}".format(self.title, self.isin, self.listing, self.redemption_date, self.de, endl)
 
-class BondBuilder():
+
+class BondBuilder:
     def __init__(self):
         self.title = ""
         self.isin = ""
         self.sector = ""
         self.listing = ""
-        self.redemtion_date = ""
+        self.redemption_date = ""
         self.de = ""
         self.duration = ""
         self.price = "" #float
@@ -70,19 +125,27 @@ class BondBuilder():
     def set_title(self, title):
         self.title = title
         return self
+
     def set_isin(self, isin):
         self.isin = isin
         return self
 
+    def set_listing(self, listing):
+        self.listing = listing
+        return self
+
+    def set_redemption_date(self, redemption_date):
+        self.redemption_date = redemption_date
+        return self
+
+    def set_de(self, de):
+        self.de = de
+        return self
+
     def build(self) -> Bond:
 
-        return Bond(self);
+        return Bond(self)
 
-james: Bond
-
-builder = BondBuilder()
-james = builder.set_isin("Hello").set_title("World").build()
-print(james)
 
 def form_record(strings) -> str:
     out_string = sep.join(strings)
@@ -91,63 +154,11 @@ def form_record(strings) -> str:
     return out_string
 
 
-def convert():
-    try:
-        f = open(in_file, "r", encoding="UTF-8")
-    except FileNotFoundError:
-        print("Converter: Input file doesn't exist!")
-        return
-    else:
-        with f:
-            string = f.read()
-
-    # TODO следующий вариант с базой для более удобной сортировки и фильтрации
-    raw_string_list = string.split(endl)
-
-    if len(raw_string_list) % params != 0:
-        print("Converter: Raw data is unaligned!")
-        return
-
-    # bonds = []
-
-    # for i in range(0, len(raw_string_list), params):
-    # 	bond = Bond(raw_string_list[i:params])
-    # 	bonds.append(bond)
-    # 	print(bond)
-
-    try:
-        with open(out_file, "w") as f:
-
-            f.write(form_record(header))
-            cnt = 0
-            bonds = []
-            """ зная, сколько полей на одну запись, делаем в выходном файле новую строку """
-            for i in range(0, len(raw_string_list), params):
-                slice = raw_string_list[i:i + params]
-                # bond = Bond(title=slice[0], sector=slice[1], isin=slice[2],
-                #             listing=slice[3],
-                #             redemtion_date=slice[4],
-                #             de=slice[5],
-                #             duration=float(slice[6]),
-                #             price=slice[7], #float(slice[7]),
-                #             coupon_type=slice[8],
-                #             coupon_percent=float(slice[9]),
-                #             coupon_total=int(slice[10]),
-                #             trading_volume=float(slice[11].replace(" ", "")),
-                #             security=slice[12],
-                #             rating1=slice[13],
-                #             rating2=slice[14]
-                #             )
-                # bonds.append(bond)
-                f.write(form_record(raw_string_list[i:i + params]))
-                cnt += 1
-
-        print("Converter: {} records have been written!".format(cnt))
-        print(bonds)
-
-    except PermissionError:
-        print("Converter: Couldn't create output file!")
-
-
 if __name__ == "__main__":
-    convert()
+    raw_bonds = RawData()
+    raw_bonds.populate(in_file)
+    raw_bonds.split()
+
+    if raw_bonds is not None:
+        csv_bonds = CsvData(raw_bonds.get_data(), params)
+        print(csv_bonds.get_bonds())
